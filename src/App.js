1739f04943703c9c63c0d8877d3706eb565110db
Igor Luciano de Lima Moreira <igor.moreira@ifsuldeminas.edu.br>
2020-10-14 21:03:44 -0300

    📦 NEW: Exibe lista com os repositórios da API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then((response) => {
+      const repositories = response.data;
+      setRepositories(repositories);
+      console.log(repositories);
+    });
+  }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -13,14 +24,19 @@ function App() {
 
   return (
     <div>
+      <h1>Repositórios:</h1>
+      <br />
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
